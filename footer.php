7c31230faced5957ad96d59e23f5123d9d311e93
Ceili Cornelison <ceili@ceilicornelison.com>
2013-08-13 17:46:34 -0500

    Footer widgetsish

--- footer.php
+++ footer.php
@@ -1,4 +1,33 @@
-      <footer>
+    <div class="row">
+        <div class="col-lg-2 text-center">
+            <h5>Share</h5>
+            <img src="img/facebook.png">
+            <img src="img/twitter.png">
+        </div>
+        <div class="col-lg-5">
+            <h5>Product List</h5>
+            <div class="col-6">
+                <ul>
+                    <li><a href="#">Colored iPhone</a></li>
+                    <li><a href="#">iPhone Colors</a></li>
+                    <li><a href="#">iPhone Screen Repair</a></li>
+                    <li><a href="#">iPhone 5 Color Swap</a></li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <ul>
+                    <li><a href="#">Pink iPhone</a></li>
+                    <li><a href="#">Change iPhone Color</a></li>
+                    <li><a href="#">iPhone 4 Color Swap</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-lg-5">
+            <p>Content here</p>
+        </div>
+    </div>
+
+    <footer>
         <p class="pull-right"><a href="#">Back to top</a></p>
         <p>© 2013 color-my-i.</p>
         <p class="text-center">
@@ -8,12 +37,12 @@
         	<a href="buy-wholesale.php">Buy Wholesale</a> · 
         	<a href="contact.php">Contact Us</a>
         </p>
-      </footer>
+    </footer>
 
-    </div><!-- /.container -->
+</div><!-- /.container -->
 
-    <script src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script>
-    <script src="js/cmi.js"></script>
-    <script src="js/holder.js"></script>
+<script src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script>
+<script src="js/cmi.js"></script>
+<script src="js/holder.js"></script>
 
 </body></html>
