59143110f14a55404fc8cbd4654ed753f2db298c
Ceili Cornelison <ceili@ceilicornelison.com>
2013-08-13 18:38:27 -0500

    Why was this file empty? git status

---
+++ iphone-4-color-swap.php
@@ -0,0 +1,35 @@
+<?php
+include "header.php";
+?>
+
+<div class="container">
+
+	<div class="row">
+
+		<div class="col-lg-9">
+			
+			<h1>iPhone 4 Color Swap</h1>
+			<hr>
+
+			<img src="img/iPhoneColorSwap.png" class="img-rounded img-responsive pull-left">
+
+			<p>Black and white are boring! Get an iPhone 4 color swap today!</p>
+
+			<p>This modification is sure to grab the attention of Apple enthusiasts everywhere you go! In addition to being a new color, your iPhone will look much newer, as all scratches, chips, and cracks on the front and back will be eliminated. Our iPhone 4 color swap is great if your front or back glass is already broken, as it will be replaced with glass in the color of your choice. All parts look and work exactly like a brand new iPhone! Even your home button will be transformed!</p>
+
+			<p>We offer Do-it-Yourself kits for $99 and professional installation for $129 including free priority shipping! Upgrade to overnight return shipping for only $10 to get your iPhone 4 back as soon as possible. Our professional service technicians will perform your color swap within 24 hours!</p>
+
+			<p>Our parts are Grade A and our service techs have performed hundreds of color swaps. Don't waste your time with the competition. You won't find a faster or cheaper iPhone 4 color swap service on the internet. If you want it done right, choose color-my-i.</p>
+				
+		</div>
+
+
+		<?php
+			include "sidebar.php";
+		?>
+
+	</div><!-- /.row -->
+
+<?php
+include "footer.php";
+?>
